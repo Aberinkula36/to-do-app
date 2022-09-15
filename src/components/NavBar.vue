4ce88f0777df3651c57e7b1165c34f7c50233f0a
Aberinkula36 <jmsaezteruel@hotmail.com>
2022-09-15 22:03:51 +0200

    signup started

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,12 +1,19 @@
 <template>
-    <nav v-if="user !== null">
+    <nav v-if="user">
         <router-link to="/">Home</router-link> |
-        <router-link to="/auth">Sign out</router-link> |
+        <router-link to="/auth" v-if="!user">Sign up |</router-link>
+        <router-link to="/logout">Log out</router-link> |
     </nav>
 </template>
 
 <script>
+import { mapState } from 'pinia';
+import userStore from '@/store/user';
+
 export default {
+  computed: {
+    ...mapState(userStore, ['user']),
+  },
   name: 'NavBar',
 };
 </script>
--- src/views/AuthView.vue
+++ src/views/AuthView.vue
@@ -1,9 +1,18 @@
 <template>
-    <h1>Esta es la vista de SignUp/In</h1>
-    <button @click="handleSignUp">SignUp</button>
-    <!-- Input email
-         Input password
-         Confirmar password -->
+  <h1>Esta es la vista de SignUp/In</h1>
+  <div class="container">
+  <div class="formulario">
+    <form @submit.prevent="handleSignUp">
+      <label for="email">Email
+        <input id="email" v-model="email">
+      </label>
+      <label for="password">Contraseña
+        <input id="password" type="password" v-model="password">
+      </label>
+      <button class="button">SignUp</button>
+    </form>
+  </div>
+  </div>
 </template>
 
 <script>
@@ -12,18 +21,34 @@ import userStore from '@/store/user';
 
 export default {
   name: 'AuthView',
+  data() {
+    return {
+      email: '',
+      password: '',
+    };
+  },
   computed: {
     ...mapState(userStore, ['user']),
   },
   methods: {
     ...mapActions(userStore, ['signUp']),
-    /* Aquí ponemos el INPUT del login: Usuario y Contraseña */
     handleSignUp() {
-      const userData = {
-        email: 'jmsaezteruel@hotmail.com', /* Sacar datos de los INPUTS */
-        password: 'pruebasignup',
+      const validarEmail = (email) => {
+        const validar = email
+          .toLowerCase()
+          .match(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/);
+        return validar;
       };
-      this.signUp(userData.email, userData.password);
+
+      if (!validarEmail(this.email)) {
+        alert('Por favor, introduzca un email válido');
+        return;
+      }
+
+      if (this.password.length < 8) {
+        alert('Por favor, introduzca una contraseña de mínimo 8 caracteres');
+      }
+      this.signUp(this.email, this.password);
     },
   },
   watch: {
@@ -36,3 +61,33 @@ export default {
   },
 };
 </script>
+
+<style>
+.container {
+  display: inline-block;
+  border: 1px solid lightblue;
+  border-radius: 20px;
+  padding: 40px;
+}
+
+.button {
+  display: flex;
+  flex-direction: column;
+  align-self: center;
+  font-size: 15px;
+  border: none;
+  border-radius: 20px;
+  margin-left: 35px;
+  margin-top: 30px;
+  padding: 15px;
+}
+
+.formulario {
+  display: inline-block;
+}
+
+label {
+  display: flex;
+  flex-direction: column;
+}
+</style>
