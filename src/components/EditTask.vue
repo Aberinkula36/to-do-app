b0de93b6af2c8fc77fb44bb52e49a61c77d54f08
Aberinkula36 <jmsaezteruel@hotmail.com>
2022-09-30 23:18:46 +0200

    problema netlify solucionado

--- src/components/EditTask.vue
+++ src/components/EditTask.vue
@@ -45,7 +45,7 @@ export default {
     async editTask() {
       try {
         await this.edit(this.taskProp.id, this.title, this.desc, this.fecha, this.isComplete);
-        this.$router.go(0);
+        this.$router.push({ path: '/' });
       } catch (e) {
         console.log(e);
       }
